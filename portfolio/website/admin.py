56cac6c3718ef9c384cf3919722f77a11d1ca0aa
olawale-kareem <olawale.kareem@decagon.dev>
2021-08-19 14:47:32 +0100

    added models

--- portfolio/website/admin.py
+++ portfolio/website/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import About
+from .models import About, Dev
 
 admin.site.register(About)
+admin.site.register(Dev)
--- portfolio/website/models.py
+++ portfolio/website/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 class About(models.Model):
-    text = models.CharField(max_length=1000)
-    
+    text = models.CharField(max_length=1000)  
     def __str__(self):
         return self.text
+
+class Dev(models.Model):
+    name = models.CharField(max_length=200, default='')
+    profile = models.CharField(max_length=200, default='')  
+    email = models.EmailField(max_length=200, default='') 
+    phone = models.CharField(max_length=20, default='')
+    address = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.name
 
    
